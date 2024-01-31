c00cf118b4ec51710a4f6cfd9f5546b9438f80c3
c_patel <chanakya1patel@gmail.com>
2024-01-30 21:09:34 -0700

    demo site
    coinebase fe
    1st choice
    dream comp

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,30 @@
 import styles from "./Section.module.css";
+import { useLottie } from 'lottie-react';
+
 
 const Section = () => {
+
+
+  const options = {
+    path: '/scenerr.json',
+    loop: true,
+    autoplay: true,
+  };
+
+
+  const style = {
+    width: 'auto', // Adjust as needed
+    height: 'auto', // Adjust as needed
+  };
+
+
+  const { View } = useLottie(options, style);
+
   return (
     <div className={styles.section}>
       <div className={styles.divcardMockupImageWrapper}>
         <div className={styles.cardMockup2xpngParent}>
-          <img
+          {/* <img
             className={styles.cardMockup2xpngIcon}
             alt=""
             src="/cardmockup2xpng@2x.png"
@@ -15,7 +34,8 @@ const Section = () => {
             loading="eager"
             alt=""
             src="/cardmirror2xpng@2x.png"
-          />
+          /> */}
+          {View}
         </div>
         <img className={styles.svgIcon} loading="eager" alt="" src="/svg.svg" />
         <div className={styles.divshape} />
